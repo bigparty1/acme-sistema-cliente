641898d136c09af1ce9751b6eba1db381b09634a
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 18:05:24 -0300

    feat: Adicionar modelo para botões de ação da tablea

---
+++ src/components/table/tableActionButtonModel.js
@@ -0,0 +1,11 @@
+class TableActionButtonModel {
+  constructor({label, action, tooltip, icon, style}) {
+    this.label = label;
+    this.action = action;
+    this.tooltip = tooltip;
+    this.icon = icon;
+    this.style = style;
+  }
+}
+
+export default TableActionButtonModel;
