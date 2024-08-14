b575457d49348c12cff5e991066176af6f18d622
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 18:52:59 -0300

    fix: Corrigir nomes das paginas

--- src/data/navegationBarData.js
+++ src/data/navegationBarData.js
@@ -3,25 +3,25 @@ import './navegationBarModel';
 import { FaHospitalUser } from "react-icons/fa";
 import { FaFileWaveform } from "react-icons/fa6";
 
-import PatientPage from '../../pages/patient/patient.jsx';
-import TreatmentPage from '../../pages/treatment/treatment.jsx';
+import PacientePage from '../../pages/pacientePage/pacientePage.jsx';
+import TratamentoPage from '../../pages/tratamentopage/tratamentopage.jsx';
 
-import navegationBarModel from '../components/navegationBar/navegationBarModel';
+import NavegationBarModel from '../components/navegationBar/navegationBarModel';
 
 const navBarData = [
-    new navegationBarModel({
+    new NavegationBarModel({
         label: 'Paciente',
         path: '/Paciente',
         icon: <FaHospitalUser/>,
         className: 'navbar-patient',
-        pageComponent: <PatientPage />
+        pageComponent: <PacientePage />
     }),
-    new navegationBarModel({
+    new NavegationBarModel({
         label: 'Tratamento',
         path: '/Tratamento',
         icon: <FaFileWaveform/>,
         className: 'navbar-treatment',
-        pageComponent: <TreatmentPage />
+        pageComponent: <TratamentoPage />
     })
 ];
 
