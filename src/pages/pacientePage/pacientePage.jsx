2254bdb911298b8ebbc973cf0d433612d0115886
Kauê Gomes <kauesgomes22@gmail.com>
2024-08-14 18:51:40 -0300

    feat: Prototipo das paginas Paciente e Tratamento

---
+++ src/pages/pacientePage/pacientePage.jsx
@@ -0,0 +1,9 @@
+import './pacientePage.css';
+
+export default function PacientePage() {
+    return (
+        <div className="paciente-page">
+            <h1>Paciente</h1>
+        </div>
+    );
+}
---
+++ src/pages/tratamentoPage/tratamentoPage.jsx
@@ -0,0 +1,9 @@
+import './tratamentoPage.css';
+
+export default function TratamentoPage() {
+    return (
+        <div className="tratamento-page">
+            <h1>Tratamento</h1>
+        </div>
+    );
+}
